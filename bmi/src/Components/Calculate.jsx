ef3ca5a69d09bf55092cc887624cad87cc947160
saran345 <saran65444@gmail.com>
2025-03-10 00:50:00 +0530

    commit01

--- bmi/src/Components/Calculate.jsx
+++ bmi/src/Components/Calculate.jsx
@@ -1,26 +1,41 @@
 import { useState } from "react"
+import Display from "./display";
 // import Display from "./display";
 
 export default function Calculate() {
 
-     const [ count,setCount]=useState([]);
+     const [ count,setCount]=useState({weight:"",height:"",bmi:null});
 
   const handleChange=(e)=>{
-    const { name, value}=e;
+    const { name, value}=e.target;
 
     setCount({...count,[name]:value})
 
   }
+  function bmical(weight,height){ 
+    if(!weight || !height) return null;
+     let calheight=height/100;
+
+    return (weight /(calheight*calheight)).toFixed(2);
+  }
+
+   const handlecalculate=()=>{
+   const find=bmical(Number(count.weight),Number(count.height));
+   setCount({...count,bmi:find});
+   console.log("value:"+find);
+   }
+
 
   return (
     <>
       <div className="flex justify-center items-center min-h-screen bg-gray-100">
-        <div className="container mx-auto p-6 max-w-lg shadow-lg rounded-lg bg-white">
-       <div>
-       <h1 className="text-2xl text-center font-bold mb-6 text-blue-500  " >BMI CALCULATOR</h1>
+        <div className="container mx-auto p-6 max-w-2xl  shadow-lg rounded-lg bg-white">
+       <div className="flex flex-col md:flex-row gap-4">
       <div className="space-y-4">
-      <input name="meter"
-      type="text" value={count.meter}
+      <h1 className="text-2xl text-center font-bold mb-6 text-blue-900  " >BMI CALCULATOR</h1>
+
+      <input name="height"
+      type="text" value={count.height}
       onChange={handleChange}
       placeholder="Enter Height(cm)"
       className="w-full p-3 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
@@ -31,7 +46,10 @@ export default function Calculate() {
       onChange={handleChange}
       className="w-full p-3 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 "
       />
-      <button className="w-full bg-blue-500 text-white py-3 rounded-md text-xl hover:bg-blue-600 transition">Calculate</button>
+      <button onClick={handlecalculate} className="w-full bg-blue-500 text-white py-3 rounded-md text-xl hover:bg-blue-600 transition">Calculate</button>
+      </div>
+      <div>
+     <Display value={count.bmi} />
       </div>
        </div>
       </div>
--- bmi/src/Components/Display.jsx
+++ bmi/src/Components/Display.jsx
@@ -30,18 +30,50 @@ function GaugePointer() {
   );
 }
 
-export default function Display() {
+ const getGaugeColor=(value)=>{
+    if(value<18.5) return "blue";
+    if(value<25) return "green";
+    if(value<30) return "orange";
+    if(value<35) return "red";
+    return "darkred";
+
+ };
+ const getLabelText=(value)=>{
+    if(value<18.5) return "Underweight";
+    if(value<25) return "Normal";
+    if(value<30) return "Overweight";
+    if(value<35) return "Obese class i";
+    return "Obese class ii";
+
+ };
+
+
+
+
+export default function Display({value}) {
+
+  const color=getGaugeColor(value);
+  const label=getLabelText(value);
+
   return (
     <GaugeContainer
       width={200}
       height={200}
       startAngle={-110}
       endAngle={110}
-      value={30}
+      value={value?Number(value):0}
     >
       <GaugeReferenceArc />
-      <GaugeValueArc />
+      <GaugeValueArc style={{fill:color}} />
       <GaugePointer />
+      <text x="100" y="110" textAnchor="middle" fontSize="20" fill="black" fontWeight="bold">
+        {value || 0}
+      </text>
+
+      <text  x="100" y="195" textAnchor="middle" fontSize="21"  fill={color} fontWeight="bold">
+        {label}
+      </text>
+
     </GaugeContainer>
   );
 }
